05a5ab9d5be83f73d0120cf36ac43c3d7b55e028
Sergei <sergei@sergei-HP-Pavilion-dv6-Notebook-PC.(none)>
2011-08-01 23:19:07 +0300

    Add "bookmark link, add items number in cart

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -42,3 +42,20 @@
 
   });
 })(jQuery);
+
+
+
+function bookmark_us(url, title){
+
+if (window.sidebar) // firefox
+    window.sidebar.addPanel(title, url, "");
+else if(window.opera && window.print){ // opera
+    var elem = document.createElement('a');
+    elem.setAttribute('href',url);
+    elem.setAttribute('title',title);
+    elem.setAttribute('rel','sidebar');
+    elem.click();
+}
+else if(document.all)// ie
+    window.external.AddFavorite(url, title);
+}
